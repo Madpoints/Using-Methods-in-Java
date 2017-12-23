6f72cfe337e50a217105da3ddc321ef782c32622
John Furtado <jf8846@gmail.com>
2017-12-23 13:37:51 -0500

    More methods and return values

--- src/com/Madpoints/Main.java
+++ src/com/Madpoints/Main.java
@@ -4,28 +4,48 @@ public class Main {
 
     public static void main(String[] args) {
         boolean gameOver = true;
-        int score = 800;
+        int score = 300;
         int levelCompleted = 5;
-        int bonus = 100;
+        int bonus = 30;
+        String playerName = "Nels";
 
-        calculateScore(gameOver, score, levelCompleted, bonus);
+        int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        displayHighScoreRank(playerName, highScoreRank(highScore));
 
-        score = 10000;
+        score = 600;
         levelCompleted = 8;
-        bonus = 200;
+        bonus = 50;
+        playerName = "Mad";
 
-        calculateScore(gameOver, score, levelCompleted, bonus);
+        highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        displayHighScoreRank(playerName, highScoreRank(highScore));
     }
 
     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
 
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
-            finalScore += 2000;
+            finalScore += 100;
             return finalScore;
         }
 
         return -1;
 
+    }
+
+    public static int highScoreRank(int highScore) {
+        if (highScore > 1000) {
+            return 1;
+        } else if (highScore > 500 && highScore <=  1000) {
+            return 2;
+        } else if (highScore > 100 && highScore <=  500) {
+            return 3;
+        } else{
+            return 4;
+        }
+    }
+
+    public static void displayHighScoreRank(String name, int rank) {
+        System.out.println("Player: " + name + " Rank: " + rank);
     }
 }
